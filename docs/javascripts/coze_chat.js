2b72100327911237f1e77a1297491e58be4bba45
MiaoReynolds <bmiao001@e.ntu.edu.sg>
2026-04-01 08:14:50 +0800

    update site content

---
+++ docs/javascripts/coze_chat.js
@@ -0,0 +1,18 @@
+<script src="https://sf-cdn.coze.com/obj/unpkg-va/flow-platform/chat-app-sdk/1.2.0-beta.6/libs/oversea/index.js"></script>
+<script>
+  new CozeWebSDK.WebChatClient({
+    config: {
+      bot_id: '7623202228375437365',
+    },
+    componentProps: {
+      title: 'Coze',
+    },
+    auth: {
+      type: 'token',
+      token: 'pat_*******',
+      onRefreshToken: function () {
+        return 'pat_*******'
+      }
+    }
+  });
+</script>
